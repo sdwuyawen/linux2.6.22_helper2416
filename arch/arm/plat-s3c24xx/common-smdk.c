e59fe90c9fba8e287da6ed178ecac66e88c3eb92
wu yawen <sdwuyawen@126.com>
2016-01-19 17:28:07 +0800

    add led0 as heartbeat

--- arch/arm/plat-s3c24xx/common-smdk.c
+++ arch/arm/plat-s3c24xx/common-smdk.c
@@ -45,12 +45,18 @@
 #include <asm/plat-s3c24xx/pm.h>
 
 /* LED devices */
-
+/*
 static struct s3c24xx_led_platdata smdk_pdata_led4 = {
 	.gpio		= S3C2410_GPF4,
 	.flags		= S3C24XX_LEDF_ACTLOW | S3C24XX_LEDF_TRISTATE,
 	.name		= "led4",
 	.def_trigger	= "timer",
+};*/
+static struct s3c24xx_led_platdata smdk_pdata_led0 = {
+	.gpio		= S3C2410_GPB1,
+	.flags		= S3C24XX_LEDF_ACTLOW | S3C24XX_LEDF_TRISTATE,
+	.name		= "led0",
+	.def_trigger	= "heartbeat",
 };
 
 static struct s3c24xx_led_platdata smdk_pdata_led5 = {
@@ -71,12 +77,19 @@ static struct s3c24xx_led_platdata smdk_pdata_led7 = {
 	.flags		= S3C24XX_LEDF_ACTLOW | S3C24XX_LEDF_TRISTATE,
 	.name		= "led7",
 };
-
+/*
 static struct platform_device smdk_led4 = {
 	.name		= "s3c24xx_led",
 	.id		= 0,
 	.dev		= {
 		.platform_data = &smdk_pdata_led4,
+	},
+};*/
+static struct platform_device smdk_led0 = {
+	.name		= "s3c24xx_led",
+	.id		= 0,
+	.dev		= {
+		.platform_data = &smdk_pdata_led0,
 	},
 };
 
@@ -236,10 +249,11 @@ static struct s3c2410_platform_nand s3c_nand_info = {
 
 static struct platform_device __initdata *smdk_devs[] = {
 	&s3c_device_nand,
-	&smdk_led4,
-	&smdk_led5,
-	&smdk_led6,
-	&smdk_led7,
+	&smdk_led0,
+//	&smdk_led4,
+//	&smdk_led5,
+//	&smdk_led6,
+//	&smdk_led7,
 };
 
 void __init smdk_machine_init(void)
@@ -255,6 +269,10 @@ void __init smdk_machine_init(void)
 	s3c2410_gpio_setpin(S3C2410_GPF5, 1);
 	s3c2410_gpio_setpin(S3C2410_GPF6, 1);
 	s3c2410_gpio_setpin(S3C2410_GPF7, 1);
+
+	/* ÅäÖÃLEDÒý½Å */
+	s3c2410_gpio_cfgpin(S3C2410_GPB1, S3C2410_GPB1_OUTP);
+	s3c2410_gpio_setpin(S3C2410_GPB1, 1);
 
 	/* ¹Ø±Õ·äÃùÆ÷ */
 	s3c2410_gpio_cfgpin(S3C2410_GPB0, S3C2410_GPB0_OUTP);
